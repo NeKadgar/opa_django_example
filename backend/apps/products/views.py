567d22cc05760212b8f9740014a7b77c30345031
Maksim Sheleh <maksim.sheleh@itechart-group.com>
2023-03-29 15:52:42 +0300

    Refactor available books select

--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from apps.products.models import Book
+from apps.users.models import User
 from apps.products.serializers import BookSerializer
 from utils.opa import opa_authorization
 
@@ -18,8 +19,12 @@ def get_book(request):
 def get_available_books(request):
     if request.user.is_superuser or request.user.is_staff:
         books = Book.objects.all()
-    else:
+    elif request.user.is_organisation_admin:
         books = Book.objects.filter(company=request.user.company)
-        if not request.user.is_organisation_admin:
-            books = books.filter(Q(access_tags__in=request.user.team.access_tags.all()) | Q(access_tags=None))
+    else:
+        books = Book.objects.filter(
+            Q(company=request.user.company) & (
+                    Q(access_tags__in=request.user.team.access_tags.all()) | Q(access_tags=None)
+            )
+        ).distinct()
     return Response(BookSerializer(books, many=True).data)
